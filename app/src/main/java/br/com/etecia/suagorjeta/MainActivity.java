e5c54ecccda47662c4036cb92b321a0761f3a544
CynGL <cyntialino09@gmail.com>
2023-10-10 22:08:59 -0300

    spinner

--- app/src/main/java/br/com/etecia/suagorjeta/MainActivity.java
+++ app/src/main/java/br/com/etecia/suagorjeta/MainActivity.java
@@ -8,17 +8,28 @@ import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+        Float res, valqs;
+        TextInputEditText valc;
+        String[] qs = {"Excelente - 10%" , "ótimo - 8%" , "Bom - 5%" , "Ruim - 2%"};
 
-        String[] qs = {"Excelente - 10%" , "òtimo - 8%" , "Bom - 5%" , "Ruim - 2%"};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //pega o valor da conta
+        valc = findViewById(R.id.valorc);
+
 
         Spinner spin = (Spinner) findViewById(R.id.spinnerqs);
         spin.setOnItemSelectedListener(this);
@@ -30,7 +41,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
     @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(),qs[position] , Toast.LENGTH_LONG).show();
+
+
 
     }
 
