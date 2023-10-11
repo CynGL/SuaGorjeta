6ebcf8fe4663c21ed75b7af9bfe5de167d649c28
CynGL <cyntialino09@gmail.com>
2023-10-10 21:36:30 -0300

    spinner

--- app/src/main/java/br/com/etecia/suagorjeta/MainActivity.java
+++ app/src/main/java/br/com/etecia/suagorjeta/MainActivity.java
@@ -6,10 +6,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
         String[] qs = {"Excelente - 10%" , "òtimo - 8%" , "Bom - 5%" , "Ruim - 2%"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spin = (Spinner) findViewById(R.id.spinnerqs);
+        spin.setOnItemSelectedListener(this);
+
+        ArrayAdapter adap = new ArrayAdapter(this,android.R.layout.simple_spinner_item,qs);
+        adap.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adap);
+    }
+
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -23,11 +41,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
 
 }
