956ae22128481e607720bff8188a5883819bc25a
CynGL <cyntialino09@gmail.com>
2023-10-10 21:27:29 -0300

    spinner

--- app/src/main/java/br/com/etecia/suagorjeta/MainActivity.java
+++ app/src/main/java/br/com/etecia/suagorjeta/MainActivity.java
@@ -3,12 +3,31 @@ package br.com.etecia.suagorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+        String[] qs = {"Excelente - 10%" , "òtimo - 8%" , "Bom - 5%" , "Ruim - 2%"};
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
